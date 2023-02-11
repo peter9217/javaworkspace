ee5b26ba12e7e502c1cdea1984c09fd01e554cdb
DESKTOP-QES8KP2\peter <peter9217@naver.com>
2023-02-11 22:42:09 +0900

    s

--- 01_Java/a001_variable/src/edu/kh/variable/PrintEx.java
+++ 01_Java/a001_variable/src/edu/kh/variable/PrintEx.java
@@ -3,7 +3,7 @@ package edu.kh.variable;
 public class PrintEx {
 	public static void main(String[] args) {
 		
-		// 출력 메서드
+		// 출력 메서드d
 		
 		// System.out.print()
 		// -> () 안의 내용을 출력 (끝나고 줄바꿈 X)
