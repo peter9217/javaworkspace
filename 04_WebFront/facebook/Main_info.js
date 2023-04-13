c9f047247c0aa8a7948af61b1bf5e3694bf24243
user1 <user1>
2023-04-13 19:46:13 +0900

    230413_오전/오후/자습_JS

---
+++ 04_WebFront/facebook/Main_info.js
@@ -0,0 +1,11 @@
+document.getElementsByClassName("info-li")
+
+function outline(){
+    document.getElementsByClassName("info-outline-1")[0].style.display="flex"
+}
+
+
+
+
+
+document.getElementsByClassName("")
